13526adbdf3440a37cd9348a2595eb5943c6313c
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2023-02-12 13:04:51 +0000

    questions and answers done

---
+++ Assets/js/questionsAnswers.js
@@ -0,0 +1,54 @@
+// Questions & answers
+
+let questions = [
+    {
+      title: `"All right Jim, your quarterlies look very good. How are things going at the library?"`,
+      choices: ["David", "Michael", "Jan", "Dwight"],
+      answer: "Michael",
+    },
+    {
+      title: `"Shall I play for you Pa Rum Pum Pum Pum, I have no gifts for you Pa Rum Pum Pum Pum."`,
+      choices: ["Angela", "Kelly", "Dwight", "Creed"],
+      answer: "Dwight",
+    },
+    {
+      title: `"You guys gotta update your resumes, just like I'm doing."`,
+      choices: ["Ryan", "Angela", "Darrel", "Oscar"],
+      answer: "Oscar",
+    },
+    {
+      title: `"So what does downsizing actually mean?"`,
+      choices: ["Kevin", "Phyllis", "Jan", "Meredith"],
+      answer: "Phyllis",
+    },
+    {
+      title: `"I bet it's gonna be me. It's probably gonna be me."`,
+      choices: ["Jim", "Pam", "Stanley", "Angela"],
+      answer: "Angela",
+    },
+    {
+      title: `"I'll drink to that. I'm starting at a company this week."`,
+      choices: ["Deangelo Vickers", "Nellie Bertram", "Robert California", "Charles Miner",],
+      answer: "Deangelo Vickers",
+    },
+    {
+      title: `"I have a customer meeting."`,
+      choices: ["David", "Kelly", "Jan", "Karen"],
+      answer: "Kelly",
+    },
+    {
+      title: `"Michael, please. Can we talk about this memo?"`,
+      choices: ["Meredith", "Kevin", "Stanley", "Creed"],
+      answer: "Meredith",
+    },
+    {
+      title: `"Oh. Okay. And to you (starts clapping)."`,
+      choices: ["Packer", "Gabe", "Erin", "Helene"],
+      answer: "Gabe",
+    },
+    {
+      title: `"You don't work in sales, do you?"`,
+      choices: ["Robert California", "Jan", "Jo Bennet", "Danny Cordray"],
+      answer: "Robert California",
+    },
+  ];
